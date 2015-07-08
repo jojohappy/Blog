eab819b16395962f0a7d4cf621e978700ce2cc84
jojohappy <daijun@hupu.com>
2015-07-08 21:49:46 +0800

    Responsive Web Design for UI

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,30 @@
-$( document ).ready(function() {
+$(document).ready(function() {
+  $("#divMenu").click(function() {
+    if($("#divMenu").hasClass('fa-arrow-left')) {
+      $("#divSearchForm").hide();
+      $("#spanBlogTitle").show();
+      $("#divSearch").show();
+      $("#divMenu").removeClass('fa-arrow-left');
+      $("#divMenu").addClass('fa-bars');
+      return;
+    }
+    if($("#divCategory").css('display') == 'none') {
+      $("#divCategory").show();
+    }
+    else {
+      $("#divCategory").hide();
+    }
+  });
+
+  $("#divSearch").click(function() {
+    if($("#divSearchForm").css('display') == 'none') {
+      $("#divSearchForm").show();
+      $("#spanBlogTitle").hide();
+      $("#divSearch").hide();
+      $("#divMenu").removeClass('fa-bars');
+      $("#divMenu").addClass('fa-arrow-left');
+      $("#divCategory").hide();
+    }
+
+  });
 });
-
-
